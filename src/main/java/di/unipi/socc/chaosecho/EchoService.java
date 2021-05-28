b3589efd1cd9bd26301fbb4e8fee4d40f66c104d
jsoldani <jsoldani@gmail.com>
2021-05-28 17:47:29 +0200

    cleaning imports

--- src/main/java/di/unipi/socc/chaosecho/EchoService.java
+++ src/main/java/di/unipi/socc/chaosecho/EchoService.java
@@ -1,10 +1,7 @@
 package di.unipi.socc.chaosecho;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class EchoService {
