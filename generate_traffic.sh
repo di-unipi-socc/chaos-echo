2f2ba452993b0e7e130460e68fa26b3e303b1d38
jsoldani <jsoldani@gmail.com>
2021-06-09 10:11:51 +0000

    ensuring all requests are handled by frontend

--- generate_traffic.sh
+++ generate_traffic.sh
@@ -5,7 +5,6 @@
 # --------------------
 A=localhost:8080 # address (and port) where to reach the application frontend
 N=1000 # amount of CURL requests to send to the application frontend (default value)
-P=0.001 # time interval, in seconds, between requests (default value)
 
 # check if options (-n, -p) are specified to customise run configuration
 while (( "$#" )); do
@@ -17,15 +16,6 @@ while (( "$#" )); do
 			else
 				echo "ERROR: Argument for $1 is missing" >&2
 				exit 1
-			fi
-		;;
-		-p|--period)
-			if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
-				P=$2
-				shift 2
-			else
-				echo "ERROR: Argument for $1 is missing" >&2
-				exit 2
 			fi
 		;;
 		-a|--address)
@@ -40,7 +30,7 @@ while (( "$#" )); do
 		*)
 			echo "ERROR: Unsupported flag $1" >&2
 			echo ""
-			echo "    Usage: ./generate_traffic [-n|--number N] [-p|--period M]"
+			echo "    Usage: ./generate_traffic [-n|--number N] [-a|--address HOSTNAME:PORT]"
 			echo ""
 			exit 3
 		;;
@@ -52,10 +42,12 @@ done
 # --------------------
 
 for i in $(seq $N); do
-	curl -X POST -H 'Content-Type: application/json' \
-		-i http://$A/echo \
-		--data '{ "content": "FRONTEND REQUEST", "hash": "1" }' \
-		&> /dev/null &
-	echo "Request $i done"
-	sleep $P
+	echo ""
+	echo "** Request $i **"
+	R=$(curl -X POST -H 'Content-Type: application/json' -H 'Cache-Control: no-cache' -i http://$A/echo --data '{ "content": "FRONTEND REQUEST", "hash": "1" }' --silent)
+	while [ -z "$R" ]; do
+		sleep 5
+		R=$(curl -X POST -H 'Content-Type: application/json' -H 'Cache-Control: no-cache' -i http://$A/echo --data '{ "content": "FRONTEND REQUEST", "hash": "1" }' --silent)
+	done
+	echo "$R"
 done
