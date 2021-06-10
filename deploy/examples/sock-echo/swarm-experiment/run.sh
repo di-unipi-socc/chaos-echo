3592470c23c20b02beae033c4708563ba5c6babe
jsoldani <jsoldani@gmail.com>
2021-06-10 09:28:28 +0200

    added varying replicas

--- deploy/examples/sock-echo/swarm-experiment/run.sh
+++ deploy/examples/sock-echo/swarm-experiment/run.sh
@@ -8,10 +8,11 @@ cp ../logstash.conf logstash.conf
 cp ../../../../generate_traffic.sh generate_traffic.sh
 chmod ugo+x generate_traffic.sh
 
-# --------------------------------------------------------------------
-# Generate logs for varying FAIL_PERCENTAGE (with PICK_PERCENTAGE=100)
-# --------------------------------------------------------------------
 sed -i "s/PICK_PERCENTAGE: 50/PICK_PERCENTAGE: 100/g" docker-compose.yml # set services to always invoke backends
+
+# -----------------------------------------
+# Generate logs for varying FAIL_PERCENTAGE
+# -----------------------------------------
 
 OLD_PERC=5
 for PERC in $(seq 10 10 90)
@@ -20,14 +21,14 @@ do
 	echo "|     FAIL_PERCENTAGE = $PERC      |"
 	echo "|-------------------------------|"
 	# Update docker-compose.yml
+	echo "* Update docker-compose.yml"
 	sed -i "s/FAIL_PERCENTAGE: $OLD_PERC/FAIL_PERCENTAGE: $PERC/g" docker-compose.yml
-	echo "* Updated docker-compose.yml"
 
 	# Deploy Docker stack (and wait for services to get online)
 	echo "* Docker stack deployment started"
 	docker stack deploy -c docker-compose.yml echo
 	echo "* Waiting for ELK stack to get online"
-	#sleep 60
+	sleep 100
 
 	# Load services
 	echo "* Loading services"
@@ -40,8 +41,8 @@ do
 
 	# Rename generate logs
 	LOG_FILE=$(ls echo-*)
-	mv $LOG_FILE fail_$PERC.log
-	echo "* Log file stored in fail_$PERC.log"
+	mv $LOG_FILE fail_perc_$PERC.log
+	echo "* Log file stored in fail_perc_$PERC.log"
 
 	# Cleaning Docker runtime
 	echo "* Cleaning Docker runtime"
@@ -50,6 +51,51 @@ do
 	sleep 60
 
 	OLD_PERC=$PERC
+done
+
+# -----------------------------------------
+#    Generate logs for varying REPLICAS
+# -----------------------------------------
+
+sed -i "s/FAIL_PERCENTAGE: $OLD_PERC/FAIL_PERCENTAGE: 50/g" docker-compose.yml # set 50 as FAIL_PERCENTAGE
+
+OLD_REPLICAS=1
+for REPLICAS in $(seq 1 1 10)
+do
+	echo "|-------------------------------|"
+	echo "|     REPLICAS = $REPLICAS      |"
+	echo "|-------------------------------|"
+	# Update docker-compose.yml
+	echo "* Update docker-compose.yml"
+	sed -i "s/replicas: $OLD_REPLICAS/replicas: $REPLICAS/g" docker-compose.yml
+	
+	# Deploy Docker stack (and wait for services to get online)
+	echo "* Docker stack deployment started"
+	docker stack deploy -c docker-compose.yml echo
+	echo "* Waiting for ELK stack to get online"
+	sleep 100
+
+	# Load services
+	echo "* Loading services"
+	./generate_traffic.sh > traffic.log
+	rm traffic.log
+
+	# Remove Docker stack
+	echo "* Undeployment of Docker stack"
+	docker stack rm echo
+
+	# Rename generate logs
+	LOG_FILE=$(ls echo-*)
+	mv $LOG_FILE fail_replicas_$REPLICAS.log
+	echo "* Log file stored in fail_replicas_$REPLICAS.log"
+
+	# Cleaning Docker runtime
+	echo "* Cleaning Docker runtime"
+	docker container prune -f
+	docker network prune -f
+	sleep 60
+
+	OLD_REPLICAS=$REPLICAS
 done
 
 # --------------------------------
