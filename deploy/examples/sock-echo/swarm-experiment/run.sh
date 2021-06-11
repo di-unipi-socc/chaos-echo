e3857c3834d756de5e75b66505bef9bebc5501b2
jsoldani <jsoldani@gmail.com>
2021-06-11 14:02:01 +0200

    tuning experiment

--- deploy/examples/sock-echo/swarm-experiment/run.sh
+++ deploy/examples/sock-echo/swarm-experiment/run.sh
@@ -1,104 +1,122 @@
 #!/bin/bash
 
-# ---------------------------------------
-# Copy deployment files in current folder
-# ---------------------------------------
-cp ../docker-compose.yml docker-compose.yml
-cp ../logstash.conf logstash.conf
-cp ../../../../generate_traffic.sh generate_traffic.sh
-chmod ugo+x generate_traffic.sh
-
-sed -i "s/PICK_PERCENTAGE: 50/PICK_PERCENTAGE: 100/g" docker-compose.yml # set services to always invoke backends
-
-# -----------------------------------------
-# Generate logs for varying FAIL_PERCENTAGE
-# -----------------------------------------
-
-OLD_PERC=5
-for PERC in $(seq 10 10 90)
+# Repeat experiments 10 times 
+for REPEAT in $(seq 1 1 10)
 do
-	echo "|-------------------------------|"
-	echo "|     FAIL_PERCENTAGE = $PERC      |"
-	echo "|-------------------------------|"
-	# Update docker-compose.yml
-	echo "* Update docker-compose.yml"
-	sed -i "s/FAIL_PERCENTAGE: $OLD_PERC/FAIL_PERCENTAGE: $PERC/g" docker-compose.yml
 
-	# Deploy Docker stack (and wait for services to get online)
-	echo "* Docker stack deployment started"
-	docker stack deploy -c docker-compose.yml echo
-	echo "* Waiting for ELK stack to get online"
-	sleep 100
+	# ---------------------------------------
+	# Copy deployment files in current folder
+	# ---------------------------------------
+	cp ../docker-compose.yml docker-compose.yml
+	cp ../logstash.conf logstash.conf
+	cp ../../../../load.sh load.sh
+	chmod ugo+x load.sh
 
-	# Load services
-	echo "* Loading services"
-	./generate_traffic.sh > traffic.log
-	rm traffic.log
+	sed -i "s/PICK_PERCENTAGE: 50/PICK_PERCENTAGE: 100/g" docker-compose.yml # set services to always invoke backends
 
-	# Remove Docker stack
-	echo "* Undeployment of Docker stack"
-	docker stack rm echo
+	# -----------------------------------------
+	# Generate logs for varying FAIL_PERCENTAGE
+	# -----------------------------------------
 
-	# Rename generate logs
-	LOG_FILE=$(ls echo-*)
-	mv $LOG_FILE fail_perc_$PERC.log
-	echo "* Log file stored in fail_perc_$PERC.log"
+	OLD_PERC=5
+	for PERC in $(seq 10 10 90)
+	do
+		echo "|-------------------------------|"
+		echo "|     FAIL_PERCENTAGE = $PERC      |"
+		echo "|-------------------------------|"
+		# Update docker-compose.yml
+		echo "* Update docker-compose.yml"
+		sed -i "s/FAIL_PERCENTAGE: $OLD_PERC/FAIL_PERCENTAGE: $PERC/g" docker-compose.yml
 
-	# Cleaning Docker runtime
-	echo "* Cleaning Docker runtime"
-	docker container prune -f
-	docker network prune -f
-	sleep 60
+		# Deploy Docker stack (and wait for services to get online)
+		echo "* Docker stack deployment started"
+		docker stack deploy -c docker-compose.yml echo
+		echo "* Waiting for ELK stack to get online"
+		sleep 100
 
-	OLD_PERC=$PERC
-done
+		# Load services
+		echo "* Loading services"
+		./load.sh > traffic.log
+		rm traffic.log
 
-# -----------------------------------------
-#    Generate logs for varying REPLICAS
-# -----------------------------------------
+		# Remove Docker stack
+		echo "* Undeployment of Docker stack"
+		docker stack rm echo
 
-sed -i "s/FAIL_PERCENTAGE: $OLD_PERC/FAIL_PERCENTAGE: 50/g" docker-compose.yml # set 50 as FAIL_PERCENTAGE
+		# Rename generate logs
+		LOG_FILE=$(ls echo-*)
+		mv $LOG_FILE fail_perc_$PERC.log
+		echo "* Log file stored in fail_perc_$PERC.log"
 
-OLD_REPLICAS=1
-for REPLICAS in $(seq 1 1 10)
-do
-	echo "|-------------------------------|"
-	echo "|     REPLICAS = $REPLICAS      |"
-	echo "|-------------------------------|"
-	# Update docker-compose.yml
-	echo "* Update docker-compose.yml"
-	sed -i "s/replicas: $OLD_REPLICAS/replicas: $REPLICAS/g" docker-compose.yml
-	
-	# Deploy Docker stack (and wait for services to get online)
-	echo "* Docker stack deployment started"
-	docker stack deploy -c docker-compose.yml echo
-	echo "* Waiting for ELK stack to get online"
-	sleep 300
+		# Cleaning Docker runtime
+		echo "* Cleaning Docker runtime"
+		docker container prune -f
+		docker network prune -f
+		sleep 60
 
-	# Load services
-	echo "* Loading services"
-	./generate_traffic.sh > traffic.log
-	rm traffic.log
+		OLD_PERC=$PERC
+	done
 
-	# Remove Docker stack
-	echo "* Undeployment of Docker stack"
-	docker stack rm echo
+	# --------------------------------
+	# Process (and clean) obtained logs
+	# --------------------------------
+	python3 avg_recovery_time.py > replicas_$REPEAT.csv
+	rm fail_*
 
-	# Rename generate logs
-	LOG_FILE=$(ls echo-*)
-	mv $LOG_FILE fail_replicas_$REPLICAS.log
-	echo "* Log file stored in fail_replicas_$REPLICAS.log"
+	# -----------------------------------------
+	#    Generate logs for varying REPLICAS
+	# -----------------------------------------
 
-	# Cleaning Docker runtime
-	echo "* Cleaning Docker runtime"
-	docker container prune -f
-	docker network prune -f
-	sleep 60
+	sed -i "s/FAIL_PERCENTAGE: $OLD_PERC/FAIL_PERCENTAGE: 50/g" docker-compose.yml # set 50 as FAIL_PERCENTAGE
 
-	OLD_REPLICAS=$REPLICAS
-done
+	OLD_REPLICAS=1
+	for REPLICAS in $(seq 1 1 10)
+	do
+		echo "|-------------------------------|"
+		echo "|     REPLICAS = $REPLICAS      |"
+		echo "|-------------------------------|"
+		# Update docker-compose.yml
+		echo "* Update docker-compose.yml"
+		sed -i "s/replicas: $OLD_REPLICAS/replicas: $REPLICAS/g" docker-compose.yml
+		
+		# Deploy Docker stack (and wait for services to get online)
+		echo "* Docker stack deployment started"
+		docker stack deploy -c docker-compose.yml echo
+		echo "* Waiting for ELK stack to get online"
+		sleep 300
 
-# --------------------------------
-# Remove (copied) deployment files
-# --------------------------------
-rm docker-compose.yml logstash.conf generate_traffic.sh
+		# Load services
+		echo "* Loading services"
+		./generate_traffic.sh > traffic.log
+		rm traffic.log
+
+		# Remove Docker stack
+		echo "* Undeployment of Docker stack"
+		docker stack rm echo
+
+		# Rename generate logs
+		LOG_FILE=$(ls echo-*)
+		mv $LOG_FILE fail_replicas_$REPLICAS.log
+		echo "* Log file stored in fail_replicas_$REPLICAS.log"
+
+		# Cleaning Docker runtime
+		echo "* Cleaning Docker runtime"
+		docker container prune -f
+		docker network prune -f
+		sleep 60
+
+		OLD_REPLICAS=$REPLICAS
+	done
+
+	# --------------------------------
+	# Remove (copied) deployment files
+	# --------------------------------
+	rm docker-compose.yml logstash.conf generate_traffic.sh
+
+	# --------------------------------
+	# Process (and clean) obtained logs
+	# --------------------------------
+	python3 avg_recovery_time.py > replicas_$REPEAT.csv
+	rm fail_*
+
+done 
---
+++ load.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# --------------------
+# RUN CONFIGURATION
+# --------------------
+A=localhost:8080 # address (and port) where to reach the application frontend (default value)
+D=3000 # time interval during which to send (non-blocking) CURL requests to the application frontend (default value: 5 minutes)
+P=0.01 # period for CURL requests (default value: 1/100 sec)
+
+# check if options (-n, -p) are specified to customise run configuration
+while (( "$#" )); do
+	case "$1" in
+		-a|--address)
+			if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+				A=$2
+				shift 2
+			else
+				echo "ERROR: Argument for $1 is missing" >&2
+				exit 2
+			fi
+		;;
+		-d|--duration)
+			if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+				D=$2
+				shift 2
+			else
+				echo "ERROR: Argument for $1 is missing" >&2
+				exit 1
+			fi
+		;;
+		-p|--period)
+			if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+				P=$2
+				shift 2
+			else
+				echo "ERROR: Argument for $1 is missing" >&2
+				exit 1
+			fi
+		;;
+		*)
+			echo "ERROR: Unsupported flag $1" >&2
+			echo ""
+			echo "    Usage: ./load.sh [-a|--address HOSTNAME:PORT] [-d|--duration SECONDS] [-p|--period SECONDS] "
+			echo ""
+			exit 3
+		;;
+	esac
+done
+
+# --------------------
+# TRAFFIC GENERATION
+# --------------------
+
+SECONDS=0
+while (( SECONDS <= D )); do  # loop until duration D (seconds) has expired
+	curl -X POST -H 'Content-Type: application/json' -H 'Cache-Control: no-cache' -i http://$A/echo --data '{ "content": "FRONTEND REQUEST", "hash": "1" }' --retry 6 --retry-connrefused &
+	sleep $P
+done
