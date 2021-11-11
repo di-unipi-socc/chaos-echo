bb1b8fb17369256265379008fa57a29f15d35f94
jsoldani <jsoldani@gmail.com>
2021-11-11 10:31:25 +0100

    removed unused import

--- src/main/java/di/unipi/socc/chaosecho/EchoServiceController.java
+++ src/main/java/di/unipi/socc/chaosecho/EchoServiceController.java
@@ -19,7 +19,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
 
